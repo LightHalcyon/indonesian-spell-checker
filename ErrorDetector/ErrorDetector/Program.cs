0477e7a0dc8b01788cd533d85bfc7464725767cf
reznov53 <adityawarman.fanaro@outlook.com>
2017-11-29 08:44:42 +0700

    Initial Commit

---
+++ ErrorDetector/ErrorDetector/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(value: "Enter file location: ");
+            String input = Console.ReadLine();
+
+            
+        }
+    }
+}

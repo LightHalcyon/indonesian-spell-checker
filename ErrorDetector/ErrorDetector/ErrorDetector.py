d5cd1ded3ef32ce4deecc63de4f86b2f8e4abdf4
Adityawarman Fanaro <reznov53@live.com>
2017-12-03 13:15:05 +0700

    Added Sastrawi stemmer incase of words needed to be stemmed

--- ErrorDetector/ErrorDetector/ErrorDetector.py
+++ ErrorDetector/ErrorDetector/ErrorDetector.py
@@ -1,14 +1,19 @@
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+
 lib = []
 with open("kebi.txt") as f:
     for line in f:
         lib.append(line.replace('\n',''))
 
-print(lib)
 data = []
 
 with open("sentence.txt",encoding='utf-8') as g:
     for line in g:
         data.append(line)
+
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
 
 typo = []
 for lineNum, line in enumerate(data,1):
@@ -16,6 +21,7 @@ for lineNum, line in enumerate(data,1):
     currLine = line.replace('.','').replace(',','').replace('\n','').lower().split()
     for wordNum, word in enumerate(currLine,1):
         if word not in lib:
-            typo.append((lineNum,wordNum))
+            if stemmer.stem(word) not in lib:
+                typo.append((lineNum,wordNum))
 
 print(typo)

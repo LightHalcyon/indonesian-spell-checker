b2aef755c02bcba501af30dab302d90866eacc92
Adityawarman Fanaro <reznov53@live.com>
2017-12-04 12:12:47 +0700

    Fixed error detection on digit and non Indonesian word

--- ErrorDetector/ErrorDetector/ErrorDetector.py
+++ ErrorDetector/ErrorDetector/ErrorDetector.py
@@ -1,27 +1,34 @@
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import enchant
+import nltk
+import re
 
 
 lib = []
 with open("kebi.txt") as f:
     for line in f:
-        lib.append(line.replace('\n',''))
+        lib.append(line.replace('\n','').lower())
 
 data = []
-
 with open("sentence.txt",encoding='utf-8') as g:
     for line in g:
         data.append(line)
 
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
+d = enchant.Dict("en_US")
+en = set(w.lower() for w in nltk.corpus.words.words())
 
 typo = []
 for lineNum, line in enumerate(data,1):
     currLine = []
-    currLine = line.replace('.','').replace(',','').replace('\n','').lower().split()
+    currLine = re.sub(r"[^A-Za-z0-9 ]","",re.sub(r"[^A-Za-z0-9\.\,\n ]"," ",line)).lower().split()
     for wordNum, word in enumerate(currLine,1):
-        if word not in lib:
-            if stemmer.stem(word) not in lib:
-                typo.append((lineNum,wordNum))
+        if not word.isdigit():
+            if word not in en:
+                if word not in lib:    
+                    stemmed = stemmer.stem(word)
+                    if stemmed not in lib:
+                        print(stemmed)
+                        typo.append((lineNum,wordNum))
 
-print(typo)

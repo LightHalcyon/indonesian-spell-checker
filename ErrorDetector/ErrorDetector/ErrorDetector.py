8d2ef862b7f86496170309b6cbe71b4a86273fe8
Adityawarman Fanaro <reznov53@live.com>
2017-12-10 21:12:01 +0700

    Added Suggestion on typo

--- ErrorDetector/ErrorDetector/ErrorDetector.py
+++ ErrorDetector/ErrorDetector/ErrorDetector.py
@@ -1,34 +1,86 @@
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-import enchant
+#import enchant
 import nltk
 import re
+from builtins import any as b_any
 
+class Typo(object):
+    lineNum = 0
+    wordNum = 0
+    errorIndexFront = 0
+    errorIndexRear = 0
+    suggestion = {}
 
-lib = []
+    def __init__(self, lineNum, wordNum):
+        self.lineNum = lineNum
+        self.wordNum = wordNum
+        self.errorIndexFront = 0
+        self.errorIndexRear = 0
+        self.suggestion = {}
+
+    def setErrorIndexFront(self, errorIndexFront):
+        self.errorIndexFront = errorIndexFront
+
+    def setErrorIndexRear(self, errorIndexRear):
+        self.errorIndexRear = errorIndexRear
+
+    def addSuggestion(self, word):
+        self.suggestion[word] = len(self.suggestion)
+
+    def getErrorIndexFront(self):
+        return self.errorIndexFront
+
+    def getErrorIndexRear(self):
+        return self.errorIndexRear
+
+lib = {}
 with open("kebi.txt") as f:
+    i = 0
     for line in f:
-        lib.append(line.replace('\n','').lower())
-
+        lib[line.replace('\n','').lower()] = i
+        i+=1
+        
 data = []
-with open("sentence.txt",encoding='utf-8') as g:
+with open("sentence2.txt",encoding='utf-8') as g:
     for line in g:
-        data.append(line)
+        data.append(re.sub(r"[^A-Za-z0-9 ]","",re.sub(r"[^A-Za-z0-9\.\,\n ]"," ",line)).lower())
 
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
-d = enchant.Dict("en_US")
+#d = enchant.Dict("en_US")
 en = set(w.lower() for w in nltk.corpus.words.words())
 
-typo = []
+typo = {}
 for lineNum, line in enumerate(data,1):
     currLine = []
-    currLine = re.sub(r"[^A-Za-z0-9 ]","",re.sub(r"[^A-Za-z0-9\.\,\n ]"," ",line)).lower().split()
+    currLine = line.split()
     for wordNum, word in enumerate(currLine,1):
         if not word.isdigit():
             if word not in en:
-                if word not in lib:    
+                if word not in lib.keys():    
                     stemmed = stemmer.stem(word)
-                    if stemmed not in lib:
-                        print(stemmed)
-                        typo.append((lineNum,wordNum))
+                    if stemmed not in lib.keys():
+                        #print(stemmed)
+                        typo[stemmed] = Typo(lineNum,wordNum)
 
+for word in typo.keys():
+    #print(word, " ", typo[word])
+    for x in range(0, len(word)):
+        if not b_any(i.startswith(word[0:x]) for i in lib.keys()):
+            typo[word].setErrorIndexFront(x)
+            print(word[0:x])
+            print(word[0:typo[word].getErrorIndexFront()-1])
+            break
+
+    for x in range(len(word)-2, 0, -1):
+        if not b_any(i.endswith(word[x:len(word)]) for i in lib.keys()):
+            typo[word].setErrorIndexRear(x)
+            print(word[x:len(word)])
+            print(word[typo[word].getErrorIndexRear()+1:len(word)])
+            break
+
+    for sug in lib.keys():
+        if sug.startswith(word[0:typo[word].getErrorIndexFront()-1]):
+            if sug.endswith(word[typo[word].getErrorIndexRear()+1:len(word)]):
+                typo[word].addSuggestion(sug)
+                #print(sug)
